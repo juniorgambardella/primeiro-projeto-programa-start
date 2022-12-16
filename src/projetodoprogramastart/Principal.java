83d2267d267fbbaf294ba3f57b6bdebab84fdae1
Júnior Gambardella <gambardellajunior@gmail.com>
2022-12-16 09:58:43 -0300

    first commit

---
+++ src/projetodoprogramastart/Principal.java
@@ -0,0 +1,8 @@
+package projetodoprogramastart;
+
+public class Principal {
+  public static void main(String[] args) {
+    System.out.println("Oi");
+    System.out.println("Aprendendo comadno de saída");
+  }
+}
